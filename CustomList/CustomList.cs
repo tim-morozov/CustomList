84be4433d7602fc57eed7509076511eeabf662d5
tim-morozov <tim.morozov@gmail.com>
2020-04-27 14:17:48 -0500

    added TestMethod

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    class CustomList
+   public class CustomList<T>
     {
     }
 }
--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace CustomListTests
 {
@@ -9,6 +10,60 @@ namespace CustomListTests
         [TestMethod]
         public void TestMethod1()
         {
+            [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        // what happens if you add multiple things (or add to a CustomList that already has some values)?
+        [TestMethod]
+        public void Add_AddingValueToListThatAlreadyHasValues()
+        { 
+            CustomList<int> testList = new CustomList<int>();
+            int addedItem = 10;
+            int itemToAdd = 20;
+            int expected = testList[1] == itemToAdd;
+            int actual;
+
+            testList.Add(addedItem);
+            testList.Add(itemToAdd);
+            actual = testList[1];
+
+            Assert.AreEqual(expected, actual);
+        }
+        // what happens to the last-added item?
+        // what happens to the Count?
+
+        // what happens if you add more items than the initial Capacity of the CustomList?
     }
+}
 }
